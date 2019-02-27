e6e0f19b9e0a9f03771658c55a5036e6fcd33c33
Manan Sharma <manansharma296@gmail.com>
2019-02-27 20:45:56 +0530

    updated q3b

--- Assignement_1/q3b.v
+++ Assignement_1/q3b.v
@@ -1,5 +1,5 @@
 `timescale 1ns / 1ps
-
+ 
 module barrelshifter(
     output reg [7:0] data_out,  //Data Output (8bits)
     input [7:0] data_in,    //Data Input (8bits)
@@ -7,33 +7,17 @@ module barrelshifter(
     input clk,               //Clock
     input clear             //Active low clear pin, used to initially set the output to zero
     );
-    reg[2:0] a;
     always @(posedge clk, negedge clear)
     begin
         if(clear==0)
-            begin
-            data_out<=8'b00000000;
-            a <= 2'b10;
-            end
+                begin
+                data_out<=8'b00000000;
+                end
         else
             begin
-                if(a==2)
-                    begin
-                    data_out <= shift[0] ? {data_in[0],data_in[7:1]} : data_in;
-                    a<=2'b01;
-                    end
-                else if(a==1)
-                    begin
-                    data_out <= shift[1] ? {data_out[1:0],data_out[7:2]}: data_out;
-                    a<=2'b00;
-                    end
-                else if(a==0)
-                    begin
-                    data_out <= shift[2] ? {data_out[3:0],data_out[7:4]} : data_out;
-                    a<=2'b11;
-                    end
-                else
-                    data_out<=data_out;
+                data_out = shift[0] ? {data_in[0],data_in[7:1]} : data_in;
+                data_out = shift[1] ? {data_out[1:0],data_out[7:2]}: data_out;
+                data_out = shift[2] ? {data_out[3:0],data_out[7:4]} : data_out;
             end
     end
 endmodule
