a570b813c09597bd8bbf43a52a3970f18e3878d8
Anirudh0707 <yani.bh@gmail.com>
2019-02-28 18:27:11 +0530

    added wdb files for assignment 1

--- Assignement_1/Q2/q2.v
+++ Assignement_1/Q2/q2.v
@@ -1,28 +1,6 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 25.02.2019 14:49:06
-// Design Name: 
-// Module Name: ALU
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module ALU(
        output reg [7:0] sum_f1,
-       output carry_f2,
        input [7:0] A,B,
        input [2:0] select
     );
@@ -32,7 +10,7 @@ module ALU(
         3'b001: sum_f1 = A - B ;
         3'b010: sum_f1 = A + 2*B ;
         3'b011: sum_f1 = A - 2*B ;
-        3'b100: sum_f1 = A[3:0] + B[3:0] ;
+        3'b100: sum_f1 = A[3:0] * B[3:0] ;
         3'b101: sum_f1 = ((A-B)>0) ? A : B ;
         3'b110: sum_f1 = A[7] ? ~A + 1 : A ;
         3'b111: sum_f1 = B ;
